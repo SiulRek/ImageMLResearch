5d054afc80b72ee449787a55f924437477e024ee
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-11 15:03:17 +0200

    Updated import name of json_instances_serializer.

--- src/preprocessing/tests/test_runner.py
+++ src/preprocessing/tests/test_runner.py
@@ -7,7 +7,7 @@ from src.preprocessing.tests.for_helpers import copy_json_exclude_entries_test
 from src.preprocessing.tests.for_helpers import recursive_type_conversion_test
 from src.preprocessing.tests.for_helpers import randomly_select_sequential_keys_test
 from src.preprocessing.tests.for_helpers import parse_and_repeat_test
-from src.preprocessing.tests.for_helpers import class_instance_serializer_test
+from preprocessing.tests.for_helpers import json_instances_serializer_test
 from src.preprocessing.tests.for_preprocessor import image_preprocessor_test
 from src.preprocessing.tests.for_preprocessor.long_pipeline_test import (
     load_long_pipeline_tests,
@@ -53,7 +53,7 @@ def load_tests(test_suite):
     test_suite.addTest(Loader.loadTestsFromModule(copy_json_exclude_entries_test))
     test_suite.addTest(Loader.loadTestsFromModule(recursive_type_conversion_test))
     test_suite.addTest(Loader.loadTestsFromModule(randomly_select_sequential_keys_test))
-    test_suite.addTest(Loader.loadTestsFromModule(class_instance_serializer_test))
+    test_suite.addTest(Loader.loadTestsFromModule(json_instances_serializer_test))
     test_suite.addTest(Loader.loadTestsFromModule(parse_and_repeat_test))
     test_suite.addTest(Loader.loadTestsFromModule(step_base_test))
     test_suite.addTest(Loader.loadTestsFromModule(image_preprocessor_test))
