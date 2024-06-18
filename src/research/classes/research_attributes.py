40faadf0616602c3243d54ea144c6a687e9424e5
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-18 15:16:56 +0200

    Update insert_research_attributes to force proper usage.

--- src/research/classes/research_attributes.py
+++ src/research/classes/research_attributes.py
@@ -74,15 +74,30 @@ class ResearchAttributes:
 def insert_research_attributes(instance, research_attributes):
     """
     Inserts the attributes from a ResearchAttributes instance into another class
-    instance.
+    instance. Retrieves all public properties from research_attributes and sets
+    them as attributes in the instance.
 
     Args:
         - instance: The class instance to insert attributes into.
         - research_attributes (ResearchAttributes): The ResearchAttributes
             instance.
     """
+
+    def is_property(attr_name):
+        cls = research_attributes.__class__
+        attr = getattr(cls, attr_name)
+        return isinstance(attr, property)
+
+    if not isinstance(research_attributes, ResearchAttributes):
+        msg = "research_attributes must be an instance of ResearchAttributes"
+        msg += f"not {type(research_attributes)}."
+        raise ValueError(msg)
+
     for attr_name in dir(research_attributes):
-        if not attr_name.startswith("_") and not callable(
-            getattr(research_attributes, attr_name)
+        attribute = getattr(research_attributes, attr_name)
+        if (
+            not attr_name.startswith("_")
+            and not callable(attribute)
+            and is_property(attr_name)
         ):
-            setattr(instance, attr_name, getattr(research_attributes, attr_name))
+            setattr(instance, "_" + attr_name, getattr(research_attributes, attr_name))
