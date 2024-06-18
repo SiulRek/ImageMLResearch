d2050a3d74627d168adba3231c4311a891272f13
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-18 11:37:51 +0200

    Added attributes for trainer.

--- src/research/classes/module_base.py
+++ src/research/classes/module_base.py
@@ -2,7 +2,29 @@ from src.data_handling.labelling.label_manager import LabelManager
 
 
 class ModuleBase:
-    """ A base class for all modules in the research package. """
+    """
+    A base class for all modules in the research package.
+
+    Attributes:
+        - dataset_container (dict): Dictionary containing datasets. When
+            creating new 'complete_dataset' is added, when splitted
+            'train_dataset', 'val_dataset', and 'test_dataset' are added.
+        - label_manager (LabelManager): LabelManager instance for handling
+            labels.
+        - label_type (str): The type of labels used: 'binary',
+            'categorical', 'multi_label', 'multi_label_multi_class',
+            'object_detection'.
+        - category_names (list): The list of category names.
+        - predictions_container (dict): Dictionary containing predictions.
+            When fitting, predictions are added. The name corresponds to the
+            dataset name replacing 'dataset' with 'predictions', e.g.
+            'train_dataset' -> 'train_predictions'.
+        - model (tf.keras.Model): The Keras model instance.
+        - training_history (tf.keras.callbacks.History): The training
+            history of the model after fitting.
+        - evaluation_metrics (dict): The evaluation metrics dicts of the
+            model after evaluating.
+    """
 
     def __init__(self, label_type, category_names=None):
         """
@@ -18,7 +40,10 @@ class ModuleBase:
         self._label_manager = LabelManager(label_type, category_names)
         self._label_type = label_type
         self._category_names = category_names
+        self._predictions_container = {}
+        self._model = None
         self._training_history = None
+        self._evaluation_metrics = None
 
     @property
     def dataset_container(self):
@@ -49,3 +74,22 @@ class ModuleBase:
     def training_history(self):
         """ The training history of the model after fitting. """
         return self._training_history
+
+    @property
+    def predictions_container(self):
+        """
+        Dictionary containing predictions. When fitting, predictions are added.
+        The name corresponds to the dataset name replacing 'dataset' with
+        'predictions', e.g. 'train_dataset' -> 'train_predictions'.
+        """
+        return self._predictions_container
+
+    @property
+    def model(self):
+        """ The Keras model instance. """
+        return self._model
+
+    @property
+    def evaluation_metrics(self):
+        """ The evaluation metrics dicts of the model after evaluating. """
+        return self._evaluation_metrics
