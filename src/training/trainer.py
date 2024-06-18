747fb5518795d923c4228fef56735922db3648ed
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-18 11:37:57 +0200

    Added trainer currently without test suite.

---
+++ src/training/trainer.py
@@ -0,0 +1,75 @@
+from research.classes.module_base import ModuleBase
+
+
+class Trainer(ModuleBase):
+    """ A class to train a Keras model using datasets from a dataset container. """
+
+    def __init__(self, label_type=None, category_names=None):
+        """
+        Initializes the Trainer with optional label type and category names.
+
+        Args:
+            - label_type (str, optional): The type of labels used.
+            - category_names (list, optional): The list of category names.
+        """
+        super().__init__(label_type, category_names)
+
+    def set_compiled_model(self, model):
+        """
+        Sets the compiled Keras model for training.
+
+        Args:
+            - model (tf.keras.Model): Compiled Keras model.
+        """
+        self._model = model
+
+    def fit_predict_evaluate(self, **kwargs):
+        """
+        Fits the model to the dataset, saves the training history, calculates
+        predictions, and evaluates the model.
+
+        Args:
+            - **kwargs: Keyword arguments for the Keras model's fit method.
+        """
+        if self.model is None:
+            msg = "A compiled model must be set before calling fit."
+            raise ValueError(msg)
+
+        train_dataset = self.dataset_container.get("train_dataset")
+        val_dataset = self.dataset_container.get("val_dataset")
+        complete_dataset = self.dataset_container.get("complete_dataset")
+        test_dataset = self.dataset_container.get("test_dataset")
+
+        if val_dataset:
+            kwargs["validation_data"] = val_dataset  # Add to fit kwargs.
+
+        if train_dataset:
+            self._training_history = self.model.fit(train_dataset, **kwargs)
+        elif complete_dataset:
+            self._training_history = self.model.fit(complete_dataset, **kwargs)
+        else:
+            msg = "Neither 'train_dataset' nor 'complete_dataset' found in dataset container."
+            raise ValueError(msg)
+
+        if train_dataset:
+            self._predictions_container["train_predictions"] = self.model.predict(
+                train_dataset
+            )
+        if val_dataset:
+            self._predictions_container["val_predictions"] = self.model.predict(
+                val_dataset
+            )
+        if complete_dataset:
+            self._predictions_container["complete_predictions"] = self.model.predict(
+                complete_dataset
+            )
+        if test_dataset:
+            self._predictions_container["test_predictions"] = self.model.predict(
+                test_dataset
+            )
+
+        self._evaluate()
+
+    def _evaluate(self):
+        msg = "Method not implemented yet."
+        raise NotImplementedError(msg)
