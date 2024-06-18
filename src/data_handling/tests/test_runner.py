b709327dc3b296e03117939c55bfc4edd5512fdd
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-18 11:00:02 +0200

    Added TestDataHandler to test to run.

--- src/data_handling/tests/test_runner.py
+++ src/data_handling/tests/test_runner.py
@@ -8,6 +8,7 @@ from src.data_handling.tests import split_dataset_test
 from src.data_handling.tests import enhance_dataset_test
 from src.data_handling.tests import tfrecord_serialization_test
 from src.testing.helpers.test_result_logger import TestResultLogger
+from src.data_handling.tests.data_handler_test import TestDataHandler
 
 ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..")
 OUTPUT_DIR = os.path.join(ROOT_DIR, r"src/data_handling/tests/outputs")
@@ -20,6 +21,7 @@ def load_tests(test_suite):
     test_suite.addTest(Loader.loadTestsFromModule(split_dataset_test))
     test_suite.addTest(Loader.loadTestsFromModule(enhance_dataset_test))
     test_suite.addTest(Loader.loadTestsFromModule(tfrecord_serialization_test))
+    test_suite.addTest(Loader.loadTestsFromTestCase(TestDataHandler))
     return test_suite
 
 
