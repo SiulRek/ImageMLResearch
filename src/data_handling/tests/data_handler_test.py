1b0e11a81404598c906d443c2522b12266e6e932
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-21 17:52:18 +0200

    Add 2 test cases to test load_dataset.

--- src/data_handling/tests/data_handler_test.py
+++ src/data_handling/tests/data_handler_test.py
@@ -22,22 +22,76 @@ class TestDataHandler(BaseTestCase):
         cls.data_handler = DataHandler()
         cls.data_handler.update_research_attributes(research_attributes)
 
-    def setUp(self):
-        super().setUp()
-        self.data_handler.load_dataset(self.jpg_dict)
+    def _create_tfrecord(self, tfrecord_path):
+        """
+        Creates a TFRecord file with sample data.
 
-    def test_load_dataset(self):
+        Args:
+            - tfrecord_path (str): The path to the TFRecord file to create.
+        """
+        with tf.io.TFRecordWriter(tfrecord_path) as writer:
+            for i in range(10):
+                image = tf.random.normal((28, 28, 1)).numpy()
+                label = i
+                example = tf.train.Example(
+                    features=tf.train.Features(
+                        feature={
+                            "image": tf.train.Feature(
+                                bytes_list=tf.train.BytesList(
+                                    value=[
+                                        tf.io.encode_jpeg(
+                                            tf.convert_to_tensor(image, dtype=tf.uint8)
+                                        ).numpy()
+                                    ]
+                                )
+                            ),
+                            "label": tf.train.Feature(
+                                int64_list=tf.train.Int64List(value=[label])
+                            ),
+                        }
+                    )
+                )
+                writer.write(example.SerializeToString())
+
+    def test_load_dataset_from_dict(self):
         """ Test creation of dataset and storage in the dataset container. """
+        self.data_handler.load_dataset(self.jpg_dict)
         self.assertIn("complete_dataset", self.data_handler.datasets_container)
         self.assertIsInstance(
             self.data_handler.datasets_container["complete_dataset"], tf.data.Dataset
         )
 
+    def test_load_dataset_from_tfrecord(self):
+        """ Test loading of dataset from TFRecord file. """
+        # Create a TFRecord file
+        tfrecord_path = os.path.join(self.temp_dir, "test.tfrecord")
+        self._create_tfrecord(tfrecord_path)
+
+        self.data_handler.load_dataset(tfrecord_path)
+        self.assertIn("complete_dataset", self.data_handler._datasets_container)
+        self.assertIsInstance(
+            self.data_handler._datasets_container["complete_dataset"], tf.data.Dataset
+        )
+
+    def test_load_dataset_from_tf_dataset(self):
+        """ Test loading of dataset from a TensorFlow Dataset. """
+        images = tf.random.normal((10, 28, 28, 1))
+        labels = tf.constant([i for i in range(10)])
+        dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+
+        self.data_handler.load_dataset(dataset)
+        self.assertIn("complete_dataset", self.data_handler._datasets_container)
+        self.assertIsInstance(
+            self.data_handler._datasets_container["complete_dataset"], tf.data.Dataset
+        )
+
     def test_enhance_dataset(self):
         """ Test enhancement of dataset and updating in the dataset container. """
-        original_dataset = list(self.data_handler.datasets_container["complete_dataset"])
-        self.data_handler.enhance_datasets(batch_size=2, shuffle=True, random_seed=42
+        self.data_handler.load_dataset(self.jpg_dict)
+        original_dataset = list(
+            self.data_handler.datasets_container["complete_dataset"]
         )
+        self.data_handler.enhance_datasets(batch_size=2, shuffle=True, random_seed=42)
         self.assertIn("complete_dataset", self.data_handler.datasets_container)
         enhanced_dataset = self.data_handler.datasets_container["complete_dataset"]
         self.assertIsInstance(enhanced_dataset, tf.data.Dataset)
@@ -58,6 +112,7 @@ class TestDataHandler(BaseTestCase):
     def test_split_dataset(self):
         """ Test splitting of dataset and storage of splits in the dataset
         container. """
+        self.data_handler.load_dataset(self.jpg_dict)
         self.data_handler.split_dataset(train_size=0.6, val_size=0.2, test_size=0.2)
         self.assertIn("train_dataset", self.data_handler.datasets_container)
         self.assertIn("val_dataset", self.data_handler.datasets_container)
@@ -71,6 +126,7 @@ class TestDataHandler(BaseTestCase):
 
     def test_save_images(self):
         """ Test saving images from dataset to specified directory. """
+        self.data_handler.load_dataset(self.jpg_dict)
         output_dir = self.temp_dir
         self.data_handler.save_images(output_dir)
         saved_files = os.listdir(output_dir)
@@ -78,6 +134,7 @@ class TestDataHandler(BaseTestCase):
 
     def test_backup_and_restore_datasets(self):
         """ Test backup and restore of datasets. """
+        self.data_handler.load_dataset(self.jpg_dict)
         self.assertTrue("complete_dataset" in self.data_handler.datasets_container)
         self.data_handler.backup_datasets()
         self.data_handler.datasets_container.pop("complete_dataset")
