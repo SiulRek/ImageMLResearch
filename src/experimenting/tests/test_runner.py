1abcdee0e092efdcbf4055b07916e13bfc5cf954
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-25 10:50:48 +0200

    Added test_runner.py for experimenting module.

---
+++ src/experimenting/tests/test_runner.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+from unittest import defaultTestLoader as Loader
+
+from src.experimenting.tests.experiment_test import TestExperiment
+from src.experimenting.tests.trial_test import TestTrial
+from src.testing.helpers.test_result_logger import TestResultLogger
+
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..")
+OUTPUT_DIR = os.path.join(ROOT_DIR, r"src/experimenting/tests/outputs")
+LOG_FILE = os.path.join(OUTPUT_DIR, "test_results.log")
+
+
+def load_tests(test_suite):
+    test_suite.addTest(Loader.loadTestsFromTestCase(TestExperiment))
+    test_suite.addTest(Loader.loadTestsFromTestCase(TestTrial))
+    return test_suite
+
+
+if __name__ == "__main__":
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    TestResultLogger(LOG_FILE)  # Initialize Test Result Logger.
+
+    test_suite = unittest.TestSuite()
+    unittest.TextTestRunner().run(load_tests(test_suite))
