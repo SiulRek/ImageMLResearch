3703d9d69b7bf302a24ad1cf8cd47929df1f345f
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-27 17:48:24 +0200

    Reflect changes in name, add test_datatype_mismatch method.

--- src/training/tests/compute_classification_metrics_test.py
+++ src/training/tests/compute_classification_metrics_test.py
@@ -5,10 +5,12 @@ import numpy as np
 import tensorflow as tf
 
 from src.testing.base.base_test_case import BaseTestCase
-from src.training.evaluating.evaluate_classification import evaluate_classification
+from src.training.evaluating.compute_classification_metrics import (
+    compute_classification_metrics,
+)
 
 
-class TestEvaluateClassification(BaseTestCase):
+class TestComputeClassificationMetrics(BaseTestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -24,15 +26,15 @@ class TestEvaluateClassification(BaseTestCase):
             log_file.write(f"{method_name}\n" + classification_report + "\n\n")
 
     def _verify_metrics_dict(self, metrics):
-        """ Verify that the metrics dictionary contains expected keys and types. """
+        """ Verify that the Metrics dictionary contains expected keys and types. """
         expected_metrics = {
             "accuracy": float,
             "precision": float,
             "recall": float,
             "f1": float,
-            "classification_report": str
+            "classification_report": str,
         }
-        
+
         for metric, expected_type in expected_metrics.items():
             self.assertIn(metric, metrics)
             self.assertIsInstance(metrics[metric], expected_type)
@@ -41,7 +43,7 @@ class TestEvaluateClassification(BaseTestCase):
         y_true = np.array([0, 1, 1, 0])
         y_pred = np.array([0, 1, 0, 0])
         class_names = ["class_0", "class_1"]
-        metrics = evaluate_classification(y_true, y_pred, class_names)
+        metrics = compute_classification_metrics(y_true, y_pred, class_names)
         self._verify_metrics_dict(metrics)
         self.log_classification_report(
             "test_numpy_arrays", metrics["classification_report"]
@@ -51,21 +53,17 @@ class TestEvaluateClassification(BaseTestCase):
         y_true = tf.constant([0, 1, 1, 0])
         y_pred = tf.constant([0, 1, 0, 0])
         class_names = ["class_0", "class_1"]
-        metrics = evaluate_classification(y_true, y_pred, class_names)
+        metrics = compute_classification_metrics(y_true, y_pred, class_names)
         self._verify_metrics_dict(metrics)
         self.log_classification_report(
             "test_binary_classification", metrics["classification_report"]
         )
 
     def test_multi_class_classification(self):
-        y_true = np.array(
-            [[1, 0, 0], [0, 1, 0], [1, 0, 0]]
-        )
-        y_pred = np.array(
-            [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-        )
+        y_true = np.array([[1, 0, 0], [0, 1, 0], [1, 0, 0]])
+        y_pred = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
         class_names = ["class_0", "class_1", "class_2"]
-        metrics = evaluate_classification(y_true, y_pred, class_names)
+        metrics = compute_classification_metrics(y_true, y_pred, class_names)
         self._verify_metrics_dict(metrics)
         self.log_classification_report(
             "test_multi_class_classification", metrics["classification_report"]
@@ -75,10 +73,20 @@ class TestEvaluateClassification(BaseTestCase):
         y_true = tf.constant([[1, 0, 1], [0, 1, 0], [1, 1, 1]])
         y_pred = tf.constant([[1, 0, 0], [0, 1, 0], [1, 0, 1]])
         class_names = ["class_0", "class_1", "class_2"]
-        metrics = evaluate_classification(y_true, y_pred, class_names)
+        metrics = compute_classification_metrics(y_true, y_pred, class_names)
         self._verify_metrics_dict(metrics)
         self.log_classification_report(
             "test_multi_label_classification", metrics["classification_report"]
+        )
+
+    def test_datatype_mismatch(self):
+        y_true = tf.constant([0, 1, 1, 0], dtype=tf.float32)
+        y_pred = tf.constant([0, 1, 0, 0], dtype=tf.uint8)
+        class_names = ["class_0", "class_1"]
+        metrics = compute_classification_metrics(y_true, y_pred, class_names)
+        self._verify_metrics_dict(metrics)
+        self.log_classification_report(
+            "test_datatype_mismatch", metrics["classification_report"]
         )
 
 
