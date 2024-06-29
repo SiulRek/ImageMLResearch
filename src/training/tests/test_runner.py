79ae5d8586a14b82e258fa1d6611e12fadbf01b3
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-29 13:54:16 +0200

    Added test_runner.py for training module.

---
+++ src/training/tests/test_runner.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+from unittest import defaultTestLoader as Loader
+
+from src.testing.helpers.test_result_logger import TestResultLogger
+from src.training.tests.compute_classification_metrics_test import (
+    TestComputeClassificationMetrics,
+)
+from src.training.tests.trainer_test import TestTrainer
+
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..")
+OUTPUT_DIR = os.path.join(ROOT_DIR, "src/training/tests/outputs")
+LOG_FILE = os.path.join(OUTPUT_DIR, "test_results.log")
+
+
+def load_tests(test_suite):
+    test_suite.addTest(Loader.loadTestsFromTestCase(TestComputeClassificationMetrics))
+    test_suite.addTest(Loader.loadTestsFromTestCase(TestTrainer))
+    return test_suite
+
+
+if __name__ == "__main__":
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    TestResultLogger(LOG_FILE)  # Initialize Test Result Logger.
+
+    test_suite = unittest.TestSuite()
+    unittest.TextTestRunner().run(load_tests(test_suite))
