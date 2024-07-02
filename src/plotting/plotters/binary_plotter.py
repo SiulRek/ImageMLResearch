bb3f4788933d73cbb0ec5b78b882680d7311890e
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-02 10:02:00 +0200

    Add binary_plotter class and test case.

---
+++ src/plotting/plotters/binary_plotter.py
@@ -0,0 +1,23 @@
+from src.plotting.functions.plot_confusion_matrix import plot_confusion_matrix
+from src.plotting.plotters.plotter import Plotter, plot_decorator
+
+
+class BinaryPlotter(Plotter):
+    """ A class for plotting images and text using research attributes. """
+
+    @plot_decorator(default_title="Confusion Matrix", default_show=True)
+    def plot_confusion_matrix(self, **general_plot_kwargs):
+        """
+        Plots the confusion matrix for binary classification.
+
+        General plot keyword arguments:
+            - title: Optional title for the plot. Defaults to 'Confusion
+                Matrix'.
+            - show: Whether to show the plot. Defaults to True.
+
+        Returns:
+            - The figure containing the confusion matrix.
+        """
+        y_true, y_pred, class_names = self._fetch_output_data()
+        fig = plot_confusion_matrix(y_true, y_pred, class_names)
+        return fig
---
+++ src/plotting/tests/binary_plotter_test.py
@@ -0,0 +1,62 @@
+import os
+import unittest
+from unittest.mock import MagicMock
+
+import matplotlib.pyplot as plt
+
+from src.plotting.plotters.binary_plotter import BinaryPlotter
+from src.research.attributes.research_attributes import ResearchAttributes
+from src.testing.base.base_test_case import BaseTestCase
+
+
+class TestBinaryPlotter(BaseTestCase):
+    """ Test suite for the BinaryPlotter class. """
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.y_true = [0, 1, 0, 1, 0, 1, 0, 1]
+        cls.y_pred = [0, 0, 0, 1, 1, 1, 0, 1]
+        cls.class_names = ["Class 0", "Class 1"]
+        cls.binary_plotter = BinaryPlotter()
+        research_attributes = ResearchAttributes(
+            label_type="binary", class_names=cls.class_names
+        )
+        cls.binary_plotter.update_research_attributes(research_attributes)
+        cls.binary_plotter._fetch_output_data = MagicMock(
+            return_value=(cls.y_true, cls.y_pred, cls.class_names)
+        )
+
+    def setUp(self):
+        super().setUp()
+        self.binary_plotter._figures = {}  # Reset figures before each test
+
+    def test_plot_confusion_matrix(self):
+        """ Test the plot_confusion_matrix method. """
+        fig = self.binary_plotter.plot_confusion_matrix(
+            title="Test Confusion Matrix", show=False
+        )
+        self.assertEqual(
+            len(self.binary_plotter._figures), 1, "The figure was not added."
+        )
+        self.assertIn(
+            "test_confusion_matrix",
+            self.binary_plotter._figures,
+            "The figure name is incorrect.",
+        )
+        fig.savefig(
+            os.path.join(self.results_dir, "binary_plotter_plot_confusion_matrix.png")
+        )
+        plt.close(fig)
+        self.assertTrue(
+            os.path.exists(
+                os.path.join(
+                    self.results_dir, "binary_plotter_plot_confusion_matrix.png"
+                )
+            ),
+            "Confusion matrix figure was not saved.",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
