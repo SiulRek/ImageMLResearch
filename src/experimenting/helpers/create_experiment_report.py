584c8903ffc92576573ece46198860d047e75291
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-06 15:52:46 +0200

    Added summary table to experiment report.

--- src/experimenting/helpers/create_experiment_report.py
+++ src/experimenting/helpers/create_experiment_report.py
@@ -4,6 +4,33 @@ from typing import Dict
 from src.experimenting.helpers.markdown_file_writer import MarkdownFileWriter
 
 
+def get_summary_table(experiment_data: Dict) -> Dict:
+    """
+    Generate a summary table for the experiment report.
+
+    Parameters:
+        - experiment_data (dict): Dictionary containing experiment data.
+
+    Returns:
+        - dict: Dictionary containing the summary table with metrics as keys
+            and trial names as sub-keys.
+    """
+    summary_table = dict()
+    chapters = dict()
+    for trial in experiment_data.get("trials", []):
+        name = trial["name"]
+        chapters[name] = f"[Chapter](#{name.lower().replace(' ', '-')})"
+        metrics = trial.get("evaluation_metrics", {})
+        for key, value in metrics.items():
+            if isinstance(value, str):
+                continue
+            if key not in summary_table:
+                summary_table[key] = {}
+            summary_table[key][name] = f"{value:.4f}"
+    summary_table["Chapters"] = chapters
+    return summary_table
+
+
 def create_experiment_report(experiment_data: Dict):
     """
     Generate a comprehensive experiment report in Markdown format and save it to
@@ -15,7 +42,7 @@ def create_experiment_report(experiment_data: Dict):
     report_path = os.path.join(experiment_data["directory"], "experiment_report.md")
     writer = MarkdownFileWriter(report_path)
 
-    # Experiment Metadata
+    # Write Experiment Metadata
     writer.write_title(f"Experiment Report: {experiment_data['name']}", level=1)
     writer.write_title("Experiment Metadata", level=2)
     writer.write_key_value("Description", experiment_data["description"])
@@ -23,7 +50,12 @@ def create_experiment_report(experiment_data: Dict):
     experiment_directory_link = writer.create_link(experiment_data["directory"], "Link")
     writer.write_key_value("Directory", experiment_directory_link)
 
-    # Trials
+    # Write Results Summary
+    writer.write_title("Results Summary", level=2)
+    summary_table = get_summary_table(experiment_data)
+    writer.write_nested_table(summary_table)
+
+    # Write Trials
     for trial in experiment_data.get("trials", []):
         writer.write_title(trial["name"], level=2)
         writer.write_key_value("Description", trial["description"])
