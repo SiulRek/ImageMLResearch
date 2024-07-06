37a6678552da7db72f4423dafb224c8959244b5c
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-06 14:54:33 +0200

    Refactored function to use MarkdownFileWriter class.

--- src/experimenting/helpers/create_experiment_report.py
+++ src/experimenting/helpers/create_experiment_report.py
@@ -1,15 +1,58 @@
-import json
 import os
+from typing import Dict
+
+from src.experimenting.helpers.markdown_file_writer import MarkdownFileWriter
 
 
-def create_experiment_report(experiment_data):
+def create_experiment_report(experiment_data: Dict):
     """
-    Creates the experiment report by saving the experiment data to a JSON file.
+    Generate a comprehensive experiment report in Markdown format and save it to
+    a file.
 
-    Args:
-        - experiment_data (dict): The experiment data to save.
+    Parameters:
+        - experiment_data (dict): Dictionary containing experiment data.
     """
-    experiment_directory = experiment_data["directory"]
-    report_file = os.path.join(experiment_directory, "experiment_report.json")
-    with open(report_file, "w", encoding="utf-8") as file:
-        json.dump(experiment_data, file, indent=4)
+    report_path = os.path.join(experiment_data["directory"], "experiment_report.md")
+    writer = MarkdownFileWriter(report_path)
+
+    # Experiment Metadata
+    writer.write_title(f"Experiment Report: {experiment_data['name']}", level=1)
+    writer.write_title("Experiment Metadata", level=2)
+    writer.write_key_value("Description", experiment_data["description"])
+    writer.write_key_value("Start Time", experiment_data["start_time"])
+    experiment_directory_link = writer.create_link(experiment_data["directory"], "Link")
+    writer.write_key_value("Directory", experiment_directory_link)
+
+    # Trials
+    for trial in experiment_data.get("trials", []):
+        writer.write_title(trial["name"], level=2)
+        writer.write_key_value("Description", trial["description"])
+        writer.write_key_value("Start Time", trial["start_time"])
+        trial_directory_link = writer.create_link(trial["directory"], "Link")
+        writer.write_key_value("Directory", trial_directory_link)
+        hyperparameter_table = {
+            param: str(value) for param, value in trial["hyperparameters"].items()
+        }
+        writer.write_title("Hyperparameters:", level=3)
+        writer.write_key_value_table(
+            hyperparameter_table, key_label="Hyperparameter", value_label="Value"
+        )
+
+        if "figures" in trial and trial["figures"]:
+            writer.write_title("Figures:", level=3)
+            for fig_name, fig_path in trial["figures"].items():
+                writer.write_figure(fig_name, fig_path)
+
+        if "evaluation_metrics" in trial and trial["evaluation_metrics"]:
+            writer.write_title("Evaluation Metrics:", level=3)
+            evaluation_metrics_table = {
+                metric: f"{value:.4f}"
+                for metric, value in trial["evaluation_metrics"].items()
+                if isinstance(value, (int, float))
+            }
+            writer.write_key_value_table(
+                evaluation_metrics_table, key_label="Metric", value_label="Value"
+            )
+
+    # Save the report
+    writer.save_file()
