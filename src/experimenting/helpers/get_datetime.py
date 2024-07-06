02e232aea585dc61079d573760d2d77c639dbcd0
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-06 15:57:43 +0200

    Added get_datetime function.

---
+++ src/experimenting/helpers/get_datetime.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+
+def get_datetime() -> str:
+    """
+    Returns the current date and time formatted as a beautiful string.
+
+    Returns:
+        - str: The current date and time formatted as 'YYYY-MM-DD HH:MM:SS'.
+    """
+    now = datetime.now()
+    formatted_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
+    return formatted_datetime
