e657c132f29075036d07550c89a61167157118e1
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-08 16:04:20 +0200

    Added test_runner for tests in researcher module.

---
+++ src/research/tests/test_runner.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+from unittest import defaultTestLoader as Loader
+
+from src.research.tests.module_level_workflow.multi_class_workflow_test import (
+    TestMultiClassWorkflow,
+)
+from src.testing.helpers.test_result_logger import TestResultLogger
+
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..")
+OUTPUT_DIR = os.path.join(ROOT_DIR, r"src/research/tests/outputs")
+LOG_FILE = os.path.join(OUTPUT_DIR, "test_results.log")
+
+
+def load_tests(test_suite):
+    test_suite.addTest(Loader.loadTestsFromTestCase(TestMultiClassWorkflow))
+    return test_suite
+
+
+if __name__ == "__main__":
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    TestResultLogger(LOG_FILE)  # Initialize Test Result Logger.
+
+    test_suite = unittest.TestSuite()
+    unittest.TextTestRunner().run(load_tests(test_suite))
