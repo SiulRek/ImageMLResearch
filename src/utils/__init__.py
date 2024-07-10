5acd9dda658be9aaed48aee65d00688cdc996eb4
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-10 11:31:53 +0200

    Add unbatch_dataset_if_batched function and test suite.

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,6 +2,7 @@ from src.utils.general.get_sample_from_distribution import (
     get_sample_from_distribution,
 )
 from src.utils.general.search_files_with_name import search_files_with_name
+from src.utils.general.unbatch_dataset_if_batched import unbatch_dataset_if_batched
 
 # This is a Windows-specific import
 from os import name
--- src/utils/tests/test_runner.py
+++ src/utils/tests/test_runner.py
@@ -4,6 +4,7 @@ from unittest import defaultTestLoader as Loader
 
 from src.testing.helpers.test_result_logger import TestResultLogger
 from src.utils.tests import get_sample_from_distribution_test
+from src.utils.tests import unbatch_dataset_if_batched_test
 
 ROOT_DIR = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", ".."
@@ -14,6 +15,7 @@ LOG_FILE = os.path.join(OUTPUT_DIR, "test_results.log")
 
 def load_tests(test_suite):
     test_suite.addTest(Loader.loadTestsFromModule(get_sample_from_distribution_test))
+    test_suite.addTest(Loader.loadTestsFromModule(unbatch_dataset_if_batched_test))
     return test_suite
 
 
