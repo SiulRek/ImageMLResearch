0ae7c626e0078c8938c12ea05d6d13ed44c2ac9f
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-16 14:56:42 +0200

    Refactored enhance_dataset to avoid incosistency between the provided operations.

--- src/data_handling/manipulation/enhance_dataset.py
+++ src/data_handling/manipulation/enhance_dataset.py
@@ -12,19 +12,17 @@ def enhance_dataset(
 ):
     """
     Enhances a TensorFlow dataset by applying shuffling, batching, prefetching,
-    and optionally repeating a specified number of times or indefinitely if
-    repeat_num is None and the repeat parameter is set to True.
+    and repeating a number of times. If a parameter is None, the corresponding
+    operation is not applied.
 
     Args:
         - dataset (tf.data.Dataset): The initial TensorFlow dataset to
             enhance.
         - batch_size (int, optional): Size of batches of data.
-        - shuffle_seed (int, optional): Seed for random shuffling, if no
-            seed is provided, no shuffling is applied.
+        - shuffle_seed (int, optional): Seed for random shuffling.
         - prefetch_buffer_size (int, optional): Number of batches to
             prefetch (default is tf.data.experimental.AUTOTUNE).
         - repeat_num (int, optional): Number of times to repeat the dataset.
-            None for indefinite repeat.
 
     Returns:
         - tf.data.Dataset: The enhanced TensorFlow dataset.
@@ -32,10 +30,11 @@ def enhance_dataset(
     if shuffle_seed is not None:
         dataset = shuffle_dataset(dataset, random_seed=shuffle_seed)
 
-    if batch_size:
+    if batch_size is not None:
         dataset = dataset.batch(batch_size)
 
-    dataset = dataset.prefetch(buffer_size=prefetch_buffer_size)
+    if prefetch_buffer_size is not None:
+        dataset = dataset.prefetch(buffer_size=prefetch_buffer_size)
 
     if repeat_num is not None:
         dataset = dataset.repeat(repeat_num)
