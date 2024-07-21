bf14df8b6d0a1100d014a369dc50452f240a6850
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-21 13:36:32 +0200

    'Updated master_test_runner.py to account for changes in test_runners.'

--- src/testing/master/master_test_runner.py
+++ src/testing/master/master_test_runner.py
@@ -1,6 +1,6 @@
 """
 This module is designed to facilitate the running of all unit tests across the
-various submodules of the PCB Defect Detection system. It uses Python's built-in
+various submodules of the Researcher system. It uses Python's built-in
 unittest framework to run tests located in the 'tests' subdirectory of each
 module.
 """
@@ -8,34 +8,46 @@ module.
 import os
 import unittest
 
-import src.data_handling.tests.test_runner as data_handling_tests
-import src.experimenting.tests.test_runner as experimenting_tests
-import src.plotting.tests.test_runner as plotting_tests
-import src.research.tests.test_runner as research_tests
+from src.data_handling.tests.test_runner import DataHandlingTestRunner
+from src.experimenting.tests.test_runner import ExperimentingTestRunner
+from src.plotting.tests.test_runner import PlottingTestRunner
+from src.preprocessing.tests.test_runner import PreprocessingTestRunner
+from src.research.tests.test_runner import ResearchTestRunner
 from src.testing.helpers.generate_test_results_message import (
     generate_test_result_message,
 )
 from src.testing.helpers.test_result_logger import TestResultLogger
-import src.training.tests.test_runner as training_tests
-import src.utils.tests.test_runner as utils_tests
+from src.training.tests.test_runner import TrainingTestRunner
+from src.utils.tests.test_runner import UtilsTestRunner
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_FILE = os.path.join(FILE_DIR, "test_results.log")
 
 
 def run_tests():
+    """
+    Runs all the tests from various submodules and aggregates the results.
+
+    Returns:
+        - str: A message summarizing the test results.
+    """
     test_suite = unittest.TestSuite()
 
-    # Load tests from test modules
-    test_suite = utils_tests.load_tests(test_suite)
-    # NOTE: preprocessing tests currently disabled for performance reasons.
-    # test_suite = preprocessing_tests.load_tests(test_suite)
-    test_suite = data_handling_tests.load_tests(test_suite)
-    test_suite = plotting_tests.load_tests(test_suite)
-    test_suite = utils_tests.load_tests(test_suite)
-    test_suite = experimenting_tests.load_tests(test_suite)
-    test_suite = training_tests.load_tests(test_suite)
-    test_suite = research_tests.load_tests(test_suite)
+    # Instantiate test runners and add their tests to the suite
+    test_runners = [
+        DataHandlingTestRunner(),
+        # NOTE: preprocessing tests currently disabled for performance reasons.
+        # PreprocessingTestRunner(),
+        ExperimentingTestRunner(),
+        PlottingTestRunner(),
+        ResearchTestRunner(),
+        TrainingTestRunner(),
+        UtilsTestRunner(),
+    ]
+
+    for runner in test_runners:
+        runner.load_tests()
+        test_suite.addTests(runner.test_suite)
 
     result = unittest.TextTestRunner(verbosity=2).run(test_suite)
 
@@ -45,6 +57,7 @@ def run_tests():
 
 
 if __name__ == "__main__":
+    """ Main execution block for running the aggregated test suite. """
 
     os.makedirs(FILE_DIR, exist_ok=True)
     TestResultLogger(LOG_FILE)
