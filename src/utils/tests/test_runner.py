6a90ca69581cd1d55cc5be61bc8b03c59e6d5ce3
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-21 13:33:12 +0200

    Uploaded correct test_runner!

--- src/utils/tests/test_runner.py
+++ src/utils/tests/test_runner.py
@@ -1,30 +1,19 @@
-from src.data_handling.tests import (
-    shuffle_dataset_test,
-    save_images_test,
-    label_manager_test,
-    create_dataset_test,
-    split_dataset_test,
-    enhance_dataset_test,
-    tfrecord_serialization_test,
-)
-from src.data_handling.tests.data_handler_test import TestDataHandler
 from src.testing.helpers.test_runner_base import TestRunnerBase
+from src.utils.tests.get_sample_from_distribution_test import (
+    TestGetSampleFromDistribution,
+)
+from src.utils.tests.unbatch_dataset_if_batched_test import TestUnbatchDatasetIfBatched
 
 
-class DataHandlingTestRunner(TestRunnerBase):
-    """ Test Runner for the Data Handling Module. """
-    
+class UtilsTestRunner(TestRunnerBase):
+    """ Test Runner for the Utils Module. """
+
     def load_tests(self):
-        self.load_test_module(create_dataset_test)
-        self.load_test_module(label_manager_test)
-        self.load_test_module(split_dataset_test)
-        self.load_test_module(enhance_dataset_test)
-        self.load_test_module(tfrecord_serialization_test)
-        self.load_test_case(TestDataHandler)
-        self.load_test_module(shuffle_dataset_test)
-        self.load_test_module(save_images_test)
+        """ Loads the tests for the Utils Module. """
+        self.load_test_case(TestUnbatchDatasetIfBatched)
+        self.load_test_case(TestGetSampleFromDistribution)
 
 
 if __name__ == "__main__":
-    runner = DataHandlingTestRunner()
+    runner = UtilsTestRunner()
     runner.run_tests()
