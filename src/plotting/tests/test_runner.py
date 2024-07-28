fded1fa6474927b3faef951fdd2f677bd48194ec
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-28 16:07:58 +0200

    Integrated TestPlotPRCurve and TestPlotROCCurve.

--- src/plotting/tests/test_runner.py
+++ src/plotting/tests/test_runner.py
@@ -4,7 +4,9 @@ from src.plotting.tests.plot_confusion_matrix_test import TestPlotConfusionMatri
 from src.plotting.tests.plot_decorator_test import TestPlotDecorator
 from src.plotting.tests.plot_images_test import TestPlotImages
 from src.plotting.tests.plot_model_summary_test import TestPlotModelSummary
+from src.plotting.tests.plot_pr_curve_test import TestPlotPRCurve
 import src.plotting.tests.plot_results_test as plot_results_test
+from src.plotting.tests.plot_roc_curve_test import TestPlotRocCurve
 from src.plotting.tests.plot_text_test import TestPlotText
 from src.plotting.tests.plot_training_histories_test import TestPlotTrainingHistories
 from src.plotting.tests.plot_training_history_test import TestPlotTrainingHistory
@@ -26,6 +28,8 @@ class PlottingTestRunner(TestRunnerBase):
         self.load_test_case(TestPlotTrainingHistories)
         self.load_test_case(TestPlotDecorator)
         self.load_test_case(TestPlotModelSummary)
+        self.load_test_case(TestPlotRocCurve)
+        self.load_test_case(TestPlotPRCurve)
         self.load_test_module(plot_results_test)
 
 
