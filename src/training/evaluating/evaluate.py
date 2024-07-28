70019766958717e4cfb1893c44c68bb16bf0a64b
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-28 11:51:57 +0200

    Add and integrated evaluate_binary_classification function and add test methods.

--- src/training/evaluating/evaluate.py
+++ src/training/evaluating/evaluate.py
@@ -28,7 +28,6 @@ def evaluate_multi_class_classification(y_true, y_pred, class_names=None):
         - 'f1' -> float
         - 'classification_report' -> dict
     """
-
     y_pred_one_hot = np.zeros_like(y_pred)
     y_pred_one_hot[np.arange(len(y_pred)), np.argmax(y_pred, axis=-1)] = 1
 
@@ -40,6 +39,46 @@ def evaluate_multi_class_classification(y_true, y_pred, class_names=None):
     report = classification_report(
         y_true, y_pred_one_hot, output_dict=True, zero_division=0, **cn_kwarg
     )
+    eval_metrics = {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1": f1_score,
+        "classification_report": report,
+    }
+    return eval_metrics
+
+
+def evaluate_binary_classification(y_true, y_pred, class_names=None):
+    """
+    Computes classification metrics from true and predicted labels for
+    evaluation. Calculates metrics for binary classification.
+
+    Args:
+        - y_true (arrayLike): True labels.
+        - y_pred (arrayLike): Predicted labels.
+        - class_names (list, optional): List with len=2 of class names. If
+            provided, the classification report will be returned with the class
+            names.
+
+    Returns:
+        - dict: Evaluation metrics. Dictionary with the following keys:
+        - 'accuracy' -> float
+        - 'precision' -> float
+        - 'recall' -> float
+        - 'f1' -> float
+        - 'classification_report' -> dict
+    """
+    accuracy = calc_accuracy(y_true, y_pred)
+    precision = calc_precision(y_true, y_pred)
+    recall = calc_recall(y_true, y_pred)
+    f1_score = calc_f1_score(y_true, y_pred)
+    cn_kwarg = {"target_names": class_names} if class_names else {}
+    y_pred_rounded = np.round(y_pred).astype(int)
+    report = classification_report(
+        y_true, y_pred_rounded, output_dict=True, zero_division=0, **cn_kwarg
+    )
+    report.pop("accuracy", None)
     eval_metrics = {
         "accuracy": accuracy,
         "precision": precision,
@@ -62,5 +101,7 @@ def get_evaluation_function(label_type):
     """
     if label_type == "multi_class":
         return evaluate_multi_class_classification
+    if label_type == "binary":
+        return evaluate_binary_classification
     msg = f"Label type '{label_type}' is not supported for evaluation."
     raise ValueError(msg)
--- src/training/tests/evaluate_test.py
+++ src/training/tests/evaluate_test.py
@@ -3,17 +3,49 @@ import unittest
 import numpy as np
 
 from src.testing.base.base_test_case import BaseTestCase
-from src.training.evaluating.evaluate import evaluate_multi_class_classification
+from src.training.evaluating.evaluate import (
+    evaluate_binary_classification,
+    evaluate_multi_class_classification,
+)
 
 
 class TestEvaluate(BaseTestCase):
 
     def _verify_metrics_dict(self, metrics, expected_metrics):
         """ Verify that the Metrics dictionary contains expected keys and types. """
-
         for metric, expected_type in expected_metrics.items():
             self.assertIn(metric, metrics)
             self.assertIsInstance(metrics[metric], expected_type)
+
+    def test_binary_classification(self):
+        y_true = np.array([0, 1, 0, 1, 0, 1, 1, 0])
+        y_pred = np.array([0, 1, 0, 1, 1, 0, 1, 0])
+        metrics = evaluate_binary_classification(y_true, y_pred)
+        expected_metrics = {
+            "accuracy": float,
+            "precision": float,
+            "recall": float,
+            "f1": float,
+            "classification_report": dict,
+        }
+        self._verify_metrics_dict(metrics, expected_metrics)
+
+    def test_binary_classification_with_class_names(self):
+        y_true = np.array([0, 1, 0, 1, 0, 1, 1, 0])
+        y_pred = np.array([0, 1, 0, 1, 1, 0, 1, 0])
+        class_names = ["class1", "class2"]
+        metrics = evaluate_binary_classification(y_true, y_pred, class_names)
+        expected_metrics = {
+            "accuracy": float,
+            "precision": float,
+            "recall": float,
+            "f1": float,
+            "classification_report": dict,
+        }
+        self._verify_metrics_dict(metrics, expected_metrics)
+        report = metrics["classification_report"]
+        self.assertIn("class1", report)
+        self.assertIn("class2", report)
 
     def test_multi_class_classification(self):
         y_true = np.array([[1, 0, 0], [0, 1, 0], [1, 0, 0]])
