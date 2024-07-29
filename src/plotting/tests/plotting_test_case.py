ec1ad5c0b33d884fc83c8592219a1fa99e2926de
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-29 15:35:33 +0200

    Add PlottingTestCase for Plotting module tests.

---
+++ src/plotting/tests/plotting_test_case.py
@@ -0,0 +1,15 @@
+from abc import ABC
+import os
+
+import matplotlib.pyplot as plt
+
+from src.testing.base.base_test_case import BaseTestCase
+
+
+class PlottingTestCase(ABC, BaseTestCase):
+    """ Base test case for the Plotting module tests. """
+
+    def _save_and_close_figure(self, figure, file_name):
+        path = os.path.join(self.results_dir, file_name)
+        figure.savefig(path)
+        plt.close(figure)
