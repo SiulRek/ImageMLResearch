40248a5ec6a9c6cbe8132b300cc69935529e41b9
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-07-29 15:39:03 +0200

    Integrated binary_workflow_tests to test_runner.

--- src/research/tests/test_runner.py
+++ src/research/tests/test_runner.py
@@ -1,6 +1,12 @@
 from src.research.tests.data_retriever_test import TestDataRetriever
+from src.research.tests.module_level_workflow.binary_workflow_test import (
+    TestBinaryModuleLevelWorkflow,
+)
 from src.research.tests.module_level_workflow.multi_class_workflow_test import (
     TestMultiClassModuleLevelWorkflow,
+)
+from src.research.tests.researcher_level_workflow.binary_workflow_test import (
+    TestBinaryResearcherLevelWorkflow,
 )
 from src.research.tests.researcher_level_workflow.multi_class_workflow_test import (
     TestMultiClassResearcherLevelWorkflow,
@@ -12,6 +18,8 @@ class ResearchTestRunner(TestRunnerBase):
     """ Test Runner for the Research Module. """
 
     def load_tests(self):
+        self.load_test_case(TestBinaryModuleLevelWorkflow)
+        self.load_test_case(TestBinaryResearcherLevelWorkflow)
         self.load_test_case(TestMultiClassModuleLevelWorkflow)
         self.load_test_case(TestMultiClassResearcherLevelWorkflow)
         self.load_test_case(TestDataRetriever)
