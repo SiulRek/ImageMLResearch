3624e31a4bd1c9805ca29cd8c3d793544c9dd85f
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-08-12 10:55:42 +0200

    Integrated TestHParamsSuggester to ExperimentingTestRunner.

--- src/experimenting/tests/test_runner.py
+++ src/experimenting/tests/test_runner.py
@@ -1,16 +1,18 @@
 from src.experimenting.tests.experiment_data_test import TestLoadExperimentData
 from src.experimenting.tests.experiment_test import TestExperiment
+from src.experimenting.tests.hparams_suggester_test import TestHParamsSuggester
 from src.experimenting.tests.trial_test import TestTrial
 from src.testing.bases.test_runner_base import TestRunnerBase
 
 
 class ExperimentingTestRunner(TestRunnerBase):
     """ Test Runner for the Experimenting Module. """
-    
+
     def load_tests(self):
         self.load_test_case(TestExperiment)
         self.load_test_case(TestTrial)
         self.load_test_module(TestLoadExperimentData)
+        self.load_test_module(TestHParamsSuggester)
 
 
 if __name__ == "__main__":
