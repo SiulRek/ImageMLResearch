858626f076e392062d4b35ae04c1d9294b5b0343
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-10-21 16:11:19 +0200

    Use update instead of direct assigment for dataset container and add datasize_argument to split_dataset method

--- src/data_handling/data_handler.py
+++ src/data_handling/data_handler.py
@@ -14,9 +14,8 @@ class DataHandler(ResearchAttributes):
 
     def __init__(self):
         """ Initializes the DataHandler. """
-        # Not initializing ResearchAttributes here,
-        # prefer call synchronize_research_attributes explicitly.
-        # super().__init__()
+        # Not initializing ResearchAttributes here, prefer call
+        # synchronize_research_attributes explicitly. super().__init__()
 
         # Initialize research attributes used in the DataHandler
         self._label_manager = None  # Read
@@ -38,7 +37,9 @@ class DataHandler(ResearchAttributes):
             - ValueError: If the dataset is not of the required format.
         """
         for sample in dataset.take(1):
-            if not isinstance(sample, tuple) or len(sample) != 2:
+            if (
+                not isinstance(sample, tuple) or len(sample) != 2
+            ):  # XXX And instead of Or?
                 msg = "Dataset must be of form (image, label)."
                 raise ValueError(msg)
             img, _ = sample
@@ -57,9 +58,9 @@ class DataHandler(ResearchAttributes):
         Args:
             - data (tf.data.Dataset, dict, pandas.DataFrame): The data to
             - load, can be: 1. A TensorFlow dataset consisting of Tuples of
-                the form (image, label) where image shape is (height, width,1|3)
-                2. a dictionary/pandas DataFrame, with key/column is 'path' and
-                'label'.
+            the form (image, label) where image shape is (height, width,1|3)
+            2. a dictionary/pandas DataFrame, with key/column is 'path' and
+            'label'.
 
         NOTE: when passing a tf.data.Dataset it is recommended to provide a
         dataset where no Keras operation, like shuffling, batching, etc., has
@@ -71,7 +72,7 @@ class DataHandler(ResearchAttributes):
         # 1. Is already of format tensorflow.data.Dataset
         if isinstance(data, tf.data.Dataset):
             self._assert_dataset_format(data)
-            self._datasets_container["complete_dataset"] = data
+            self._datasets_container.update({"complete_dataset": data})
             return
         # 2. data is type dictslist of dicts or pandas.DataFrame
         try:
@@ -79,7 +80,7 @@ class DataHandler(ResearchAttributes):
                 data, self._label_manager.label_type, self._label_manager.class_names
             )
             self._assert_dataset_format(dataset)
-            self._datasets_container["complete_dataset"] = dataset
+            self._datasets_container.update({"complete_dataset": dataset})
             return
         except ValueError as exc:
             msg = f"Not possible to load dataset from {data}."
@@ -137,9 +138,9 @@ class DataHandler(ResearchAttributes):
                 prefetch_buffer_size=prefetch_buffer_size,
                 repeat_num=repeat_num,
             )
-            self._datasets_container[dataset_name] = enhanced_dataset
+            self._datasets_container.update({dataset_name: enhanced_dataset})
 
-    def split_dataset(self, train_size, val_size, test_size):
+    def split_dataset(self, train_size, val_size, test_size, dataset_size=None):
         """
         Splits the 'complete_dataset' into 'train_dataset', 'val_dataset' and
         'test_dataset' and stores them in the 'datasets_container'. Note that
@@ -151,16 +152,22 @@ class DataHandler(ResearchAttributes):
             - val_size (float): The proportion of the dataset for
                 validation.
             - test_size (float): The proportion of the dataset for testing.
+            - dataset_size (int, optional): The size of the dataset. If
+                None, the dataset size is determined by calling the
+                'cardinality' method.
         """
         self._assert_dataset_exists("complete_dataset")
         dataset = self._datasets_container["complete_dataset"]
         train_dataset, val_dataset, test_dataset = split_dataset(
-            dataset, train_size, val_size, test_size
+            dataset, train_size, val_size, test_size, dataset_size
         )
-        self._datasets_container["train_dataset"] = train_dataset
-        self._datasets_container["val_dataset"] = val_dataset
-        self._datasets_container["test_dataset"] = test_dataset
-
+        self._datasets_container.update(
+            {
+                "train_dataset": train_dataset,
+                "val_dataset": val_dataset,
+                "test_dataset": test_dataset,
+            }
+        )
         self._datasets_container.pop("complete_dataset")
 
     def save_images(self, output_dir, prefix=None, num_images=None):
@@ -209,14 +216,15 @@ class DataHandler(ResearchAttributes):
     def backup_datasets(self):
         """ Backups the current dataset container. """
         for key, dataset in self._datasets_container.items():
-            # Create a copy of the dataset
             for sample in dataset.take(1):
                 if isinstance(sample, tuple):
-                    self._backuped_datasets_container[key] = dataset.map(
-                        lambda x, y: (x, y)
+                    self._backuped_datasets_container.update(
+                        {key: dataset.map(lambda x, y: (x, y))}
                     )
                 else:
-                    self._backuped_datasets_container[key] = dataset.map(lambda x: x)
+                    self._backuped_datasets_container.update(
+                        {key: dataset.map(lambda x: x)}
+                    )
 
     def restore_datasets(self):
         """
@@ -228,5 +236,5 @@ class DataHandler(ResearchAttributes):
         if not self._backuped_datasets_container:
             msg = "No backuped dataset container found."
             raise ValueError(msg)
-        self._datasets_container = self._backuped_datasets_container
-        self._backuped_datasets_container = {}
+        self._datasets_container.update(self._backuped_datasets_container)
+        self._backuped_datasets_container.clear()
