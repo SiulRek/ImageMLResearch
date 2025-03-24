aa442dd730e9cc5ddcd3074559bc9e445e031541
SiulRek <luis.kraker@edu.fh-joanneum.at>
2025-03-24 11:08:37 -0500

    Add script to build Python package with pip

---
+++ pip_build.py
@@ -0,0 +1,124 @@
+import contextlib
+import os
+import shutil
+import subprocess
+import sys
+
+from imlresearch.src.testing.master.master_test_runner import run_tests
+
+ROOT_DIR = os.getcwd()
+HOME_DIR = os.path.expanduser("~")
+BUILD_DIR = os.path.join(HOME_DIR, "tmp", "imlresearch_builds")
+DIST_DIRS = ["dist", "imlresearch.egg-info"]
+
+REPLACE_MAP = {
+    ".src": "._src",
+    r"/src": r"/_src",
+    '"src': '"_src',
+}
+
+
+class BuildFailed(Exception):
+    pass
+
+
+def run_unit_tests():
+    print("Running unit tests...")
+    result = run_tests()
+    if len(result.errors) + len(result.failures) > 0:
+        raise BuildFailed("Some unit tests failed!")
+    print("All unit tests passed!")
+
+
+def run_ruff_checks():
+    print("Running ruff checks...")
+    output = subprocess.check_output("ruff check imlresearch/src", shell=True)
+    if output != b"All checks passed!\n":
+        raise BuildFailed("Some ruff checks failed!:\n", output)
+    print("All ruff checks passed!")
+
+
+def delete_build_artifacts():
+    for dist_dir in DIST_DIRS:
+        if os.path.exists(dist_dir):
+            shutil.rmtree(dist_dir)
+    if os.path.exists(BUILD_DIR):
+        shutil.rmtree(BUILD_DIR)
+
+
+def refactor_build_dir():
+    for item in os.listdir(f"{BUILD_DIR}/imlresearch/api"):
+        shutil.move(
+            os.path.join(f"{BUILD_DIR}/imlresearch/api", item),
+            f"{BUILD_DIR}/imlresearch",
+        )
+    os.rmdir(f"{BUILD_DIR}/imlresearch/api")
+
+
+def remove_tests_from_codebase():
+    codebase_dir = f"{BUILD_DIR}/imlresearch"
+    test_dirnames = ["tests", "testing"]
+    test_dirs = []
+    for root, dirs, _ in os.walk(codebase_dir):
+        for test_dirname in test_dirnames:
+            if test_dirname in dirs:
+                test_dir = os.path.join(root, test_dirname)
+                test_dirs.append(test_dir)
+    for dir_ in test_dirs:
+        shutil.rmtree(dir_)
+
+
+def setup_build_dir(exclude_tests=True):
+    if not os.path.exists(BUILD_DIR):
+        os.makedirs(BUILD_DIR)
+    shutil.copytree("imlresearch", f"{BUILD_DIR}/imlresearch")
+    required_files = ["pyproject.toml", "README.md", "LICENSE"]
+    for file in required_files:
+        shutil.copy(file, BUILD_DIR)
+
+    refactor_build_dir()
+
+    if exclude_tests:
+        remove_tests_from_codebase()
+
+
+@contextlib.contextmanager
+def set_temporary_cwd(path):
+    old_cwd = os.getcwd()
+    os.chdir(path)
+    site_packages_path = os.path.join(
+        ROOT_DIR, "venv/lib64/python3.10/site-packages"
+    )
+    pth_file = os.path.join(site_packages_path, "ImageMLResearch.pth")
+    with open(pth_file, "r", encoding="utf-8") as f:
+        previous_content = f.read()
+    try:
+        with open(pth_file, "w", encoding="utf-8") as f:
+            f.write(path)
+        yield
+    finally:
+        os.chdir(old_cwd)
+        with open(pth_file, "w", encoding="utf-8") as f:
+            f.write(previous_content)
+
+
+def exclude_tests_query():
+    exclude_tests = input(
+        "Do you want to exclude tests from the build? (y/n): "
+    )
+    return exclude_tests.lower() in ["y", "yes"]
+
+
+def build_package():
+    run_unit_tests()
+    run_ruff_checks()
+    delete_build_artifacts()
+    setup_build_dir(exclude_tests=exclude_tests_query())
+    with set_temporary_cwd(BUILD_DIR):
+        os.system(f"{sys.executable} -m build")
+    print("\n", "*" * 50)
+    print(f"Build package is available at {BUILD_DIR}")
+
+
+if __name__ == "__main__":
+    build_package()
